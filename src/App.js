2b73982dd0811fff1f00f5c8608e9a51af0e99e8
Denis <denis.karabanov@ipsoft.com>
2020-11-11 17:21:01 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Byedad11 there!!!
+           This is the end my friend the end my friend ...
         </p>
         <a
           className="App-link"
